565966464138e60432f867306acca627c487028a
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-02 13:29:32 +0300

    Update entrypoint.sh

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -4,9 +4,14 @@
 #sh -c "python 123.py"
 
 #whereis tree
+echo "First tree execute"
+echo "----"
 tree -L 1
+echo "Install packages"
 pip install sync-folders flask
 pip freeze > requirements.txt
+echo "Second tree execute"
 tree -L 1
+echo "Output requirements.txt"
 cat requirements.txt
 #echo "Hello Max"
