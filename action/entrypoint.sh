ce372f1f5a754a266ab20059c6e1ed176b4b3d8c
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-02 13:34:02 +0300

    Some beautifying

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -8,10 +8,13 @@ echo "First tree execute"
 echo "----"
 tree -L 1
 echo "Install packages"
+echo "----"
 pip install sync-folders flask
 pip freeze > requirements.txt
 echo "Second tree execute"
+echo "----"
 tree -L 1
 echo "Output requirements.txt"
+echo "----"
 cat requirements.txt
 #echo "Hello Max"
