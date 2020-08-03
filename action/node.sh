9b2de4be2138b05eb2177236cb95fc766eeef72f
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-03 09:55:54 +0300

    Try to install nodejs

--- action/node.sh
+++ action/node.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "First test from node.sh"
+echo "Second test from node.sh"
+echo "---"
+
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install npm
+npm i @mezgoodle/caesar-and-vigenere-ciphers
+npm -g list
+echo "end of node.sh"
