e7ac7653d6f40b42650f748f36ec5ce632ae18b4
Nyukers <26322892+nyukers@users.noreply.github.com>
2020-08-03 16:38:54 +0300

    Update python.sh

--- action/python.sh
+++ action/python.sh
@@ -4,19 +4,19 @@
 #sh -c "python 123.py"
 
 #whereis tree
-echo "Start node.sh"
+echo "Start python.sh"
 echo "----"
-echo "First tree execute"
+echo "First Tree execution"
 echo "----"
 tree -L 1
 echo "Install packages"
 echo "----"
 pip install sync-folders flask
 pip freeze > requirements.txt
-echo "Second tree execute"
+echo "Second Tree execution"
 echo "----"
 tree -L 1
-echo "Output requirements.txt"
+echo "Output to requirements.txt"
 echo "----"
 cat requirements.txt
 echo "End of python.sh"
