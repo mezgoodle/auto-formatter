1568afed771d595e789365195e39f304798996e5
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-04 10:14:13 +0300

    Update gitstart.sh

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -2,6 +2,11 @@
 
 echo "Start gitstart.sh"
 echo "---"
-echo "Type git clone"
+echo "Execute git clone"
+echo "---"
 git clone https://github.com/mezgoodle/sync-folders.git
+cd sync-folders
+echo "Execute cat LICENSE"
+echo "---"
+cat LICENSE
 echo "End of gitstart.sh"
