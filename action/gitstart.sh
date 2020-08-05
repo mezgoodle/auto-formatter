4519b1e8f97536d2a860816a380f20714b0c61a5
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-05 08:36:31 +0300

    Work with token

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -4,12 +4,15 @@ echo "Start gitstart.sh"
 echo "---"
 echo "Execute git clone"
 echo "---"
-git clone https://github.com/mezgoodle/sync-folders.git
-cd sync-folders
+git clone https://github.com/mezgoodle/SQL.git
+cd SQL
 echo "Execute cat LICENSE"
 echo "---"
 cat LICENSE
 echo "Execute git status"
 echo "---"
 git status
+echo "Echo repo full name"
+echo "---"
+echo $REPO_FULLNAME
 echo "End of gitstart.sh"
