2898ad65996c52bf9f6c4a79b8ac4a777e64d991
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-05 08:54:18 +0300

    Update gitstart.sh

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -2,13 +2,32 @@
 
 echo "Start gitstart.sh"
 echo "---"
-echo "Execute git clone"
+echo "Echo repo full name"
 echo "---"
-git clone https://github.com/mezgoodle/SQL.git
-cd SQL
-echo "Execute cat index.sql"
+REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+echo $REPO_FULLNAME
+echo "## Initializing git repo..."
 echo "---"
-cat index.sql
+git init
+echo "### Adding git remote..."
+echo "---"
+git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+echo "### Getting branch"
+echo "---"
+BRANCH=${GITHUB_REF#*refs/heads/}
+echo "### git fetch $BRANCH ..."
+echo "---"
+git fetch origin $BRANCH
+echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
+echo "---"
+git checkout $BRANCH
+# echo "Execute git clone"
+# echo "---"
+# git clone https://github.com/mezgoodle/SQL.git
+# cd SQL
+# echo "Execute cat index.sql"
+# echo "---"
+# cat index.sql
 echo "Create new file"
 echo "---"
 echo "hello" > test.txt
@@ -34,8 +53,4 @@ git commit -m "Formatted code" || true
 echo "## Pushing to $BRANCH"
 echo "---"
 git push -u origin $BRANCH
-echo "Echo repo full name"
-echo "---"
-REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
-echo $REPO_FULLNAME
 echo "End of gitstart.sh"
