34a2c3b70d516b6b0e6ceb24d423c2d21dc5b4ad
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-05 09:03:04 +0300

    Update gitstart.sh

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
+set -e
+
 echo "Start gitstart.sh"
 echo "---"
-# echo "Echo repo full name"
-# echo "---"
-# REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
-# echo $REPO_FULLNAME
-echo "## Initializing git repo..."
+echo "Execute git clone"
 echo "---"
-git init
-echo "### Adding git remote..."
+git clone https://github.com/mezgoodle/SQL.git
+cd SQL
+echo "Execute cat index.sql"
 echo "---"
-git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/SQL.git
-echo "### Getting branch"
-echo "---"
-BRANCH=${GITHUB_REF#*refs/heads/}
-echo "### git fetch $BRANCH ..."
-echo "---"
-git fetch origin $BRANCH
-echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
-echo "---"
-git checkout $BRANCH
-# echo "Execute git clone"
-# echo "---"
-# git clone https://github.com/mezgoodle/SQL.git
-# cd SQL
-# echo "Execute cat index.sql"
-# echo "---"
-# cat index.sql
+cat index.sql
 echo "Create new file"
 echo "---"
 echo "hello" > test.txt
@@ -44,7 +27,7 @@ git add .
 echo "## Commiting files..."
 echo "---"
 git commit -m "Formatted code" || true
-echo "## Pushing to $BRANCH"
+echo "## Pushing to master"
 echo "---"
-git push -u origin $BRANCH
+git push -u origin master
 echo "End of gitstart.sh"
