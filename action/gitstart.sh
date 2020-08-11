1f057b7a071026db0cb2e0500267d58c8a9c26c4
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-11 20:38:59 +0300

    Update gitstart.sh

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 set -e
-echo $FIRST_NAME
-REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+# echo $FIRST_NAME
+# REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 
 echo "## Initializing git repo..."
-git init
+git clone https://github.com/mezgoodle/SQL.git
 echo "### Adding git remote..."
-git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
-echo "### Getting branch"
-BRANCH=${GITHUB_REF#*refs/heads/}
-echo "### git fetch $BRANCH ..."
-git fetch origin $BRANCH
-echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
-git checkout $BRANCH
-
-echo "## Login into git..."
+git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/SQL.git
+echo "### Make commit"
+echo "hello" > test.txt
+echo "### git add ..."
+git add .
+echo "### git commit ..."
+git commit -m "test commit"
+echo "### git status"
+git status
+echo "### Login into git..."
 git config --global user.email "formatter@1337z.ninja"
 git config --global user.name "Node Code Formatter"
+echo "### Push commit"
+git push master format
