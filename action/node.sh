50113fccff290b1df0ba24011513ed7f40cbcba5
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-11 10:54:31 +0300

    Test eslint

--- action/node.sh
+++ action/node.sh
@@ -8,9 +8,7 @@ npm init -y
 echo "First list output"
 npm list
 echo "Install package"
-npm i @mezgoodle/caesar-and-vigenere-ciphers --save
-echo "Second list output"
-npm list
-echo "Output package.json"
-cat package.json
+npm i --save-dev
+echo "Init config"
+npx eslint --init
 echo "End of node.sh"
