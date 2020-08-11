1b8f8aa8dae253c0fdab763e5461db6dd8099364
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-11 13:16:30 +0300

    Some updates

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -34,7 +34,6 @@ echo "---"
 git branch
 echo "Push to master"
 echo "---"
-echo $GITHUB_ACTION
 echo $GITHUB_TOKEN
 git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/SQL.git
 git push -u format master
