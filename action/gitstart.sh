691a127a5444b77d7e9a4a1543d95fe118dc3f40
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-11 14:35:06 +0300

    Update gitstart.sh

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -4,37 +4,5 @@ set -e
 
 echo "Start gitstart.sh"
 echo "---"
-echo "Execute git clone"
-echo "---"
-git clone https://github.com/mezgoodle/SQL.git
-cd SQL
-echo "Execute cat index.sql"
-echo "---"
-cat index.sql
-echo "Create new file"
-echo "---"
-echo "hello" > test.txt
-echo "Execute git status"
-echo "---"
-git status
-echo "## Login into git..."
-echo "---"
-git config --global user.email "mezgoodle@gmail.com"
-git config --global user.name "mezgoodle"
-echo "## Staging changes..."
-echo "---"
-git add .
-echo "## Commiting files..."
-echo "---"
-git commit -m "Formatted code" || true
-echo "## Pushing to master"
-echo "---"
-echo "## Execute git branch"
-echo "---"
-git branch
-echo "Push to master"
-echo "---"
-sh -c "echo Hello world my name is $GITHUB_TOKEN"
-git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/SQL.git
-git push -u format master
+echo $GITHUB_TOKEN
 echo "End of gitstart.sh"
