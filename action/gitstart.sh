da80a9aa81c24ba9297864bddc870cd867110e68
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-13 16:15:40 +0300

    Try with autopep8

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -11,12 +11,14 @@ git clone https://github.com/mezgoodle/SQL.git
 cd SQL
 echo "### Adding git remote..."
 git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/SQL.git
-echo "### Make commit"
-echo "hello world" > test.txt
+echo "### Install autopep8"
+pip install autopep8
+echo "### Execute autopep8"
+autopep8 --in-place --recursive --aggressive --agressive .
 echo "### git add ..."
 git add .
 echo "### git commit ..."
-git commit -m "test commit"
+git commit -m "test autopep8"
 echo "### git status"
 git status
 echo "### Push commit"
