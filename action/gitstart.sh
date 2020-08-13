e858ddb5699c815f283f25fe70e48f64834dc643
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-13 16:12:06 +0300

    Update gitstart.sh

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -9,15 +9,15 @@ git config --global user.name "mezgoodle"
 echo "## Initializing git repo..."
 git clone https://github.com/mezgoodle/SQL.git
 cd SQL
-# echo "### Adding git remote..."
-# git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/SQL.git
-# echo "### Make commit"
-# echo "hello world" > test.txt
-# echo "### git add ..."
-# git add .
-# echo "### git commit ..."
-# git commit -m "test commit"
-# echo "### git status"
-# git status
-# echo "### Push commit"
-# git push format master 
+echo "### Adding git remote..."
+git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/SQL.git
+echo "### Make commit"
+echo "hello world" > test.txt
+echo "### git add ..."
+git add .
+echo "### git commit ..."
+git commit -m "test commit"
+echo "### git status"
+git status
+echo "### Push commit"
+git push format master 
