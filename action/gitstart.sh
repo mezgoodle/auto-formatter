3a5fa03b0046021db49273fbd6c2f81885e46033
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-13 16:09:50 +0300

    Update gitstart.sh

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -4,20 +4,20 @@ set -e
 # echo $FIRST_NAME
 # REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 echo "### Login into git..."
-git config --global user.email "mezgoodle@gmail.com"
-git config --global user.name "mezgoodle"
+/usr/bin/git config --global user.email "mezgoodle@gmail.com"
+/usr/bin/git config --global user.name "mezgoodle"
 echo "## Initializing git repo..."
-git clone https://github.com/mezgoodle/SQL.git
+/usr/bin/git clone https://github.com/mezgoodle/SQL.git
 cd SQL
-echo "### Adding git remote..."
-git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/SQL.git
-echo "### Make commit"
-echo "hello world" > test.txt
-echo "### git add ..."
-git add .
-echo "### git commit ..."
-git commit -m "test commit"
-echo "### git status"
-git status
-echo "### Push commit"
-git push format master 
+# echo "### Adding git remote..."
+# git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/SQL.git
+# echo "### Make commit"
+# echo "hello world" > test.txt
+# echo "### git add ..."
+# git add .
+# echo "### git commit ..."
+# git commit -m "test commit"
+# echo "### git status"
+# git status
+# echo "### Push commit"
+# git push format master 
