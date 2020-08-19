9e3e1f7fc2f37ef4d94b9dccb89435c7c67e2f1d
Nyukers <26322892+nyukers@users.noreply.github.com>
2020-08-19 15:27:24 +0300

    Create entrypoint.sh

---
+++ action/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+# $0 is a script name, 
+# $1, $2, $3 etc are passed arguments
+# $1 is our command
+CMD=$1
+
+case "$CMD" in
+  "dev" )
+    echo "dev"
+    export NODE_ENV=development
+    #exec npm run dev
+    ;;
+
+  "start" )
+    # we can modify files here, using ENV variables passed in 
+    # "docker create" command. It can't be done during build process.
+    echo "start"
+    export NODE_ENV=production
+    #exec npm start
+    ;;
+
+   * )
+    # Run custom command. Thanks to this line we can still use 
+    # "docker run our_image /bin/bash" and it will work
+    #exec $CMD ${@:2}
+    ;;
+esac
