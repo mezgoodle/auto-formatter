eac8e7d1686e92c04ecea19f453bf1be475ff590
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-20 09:07:34 +0300

    Update clean-html.js

--- utils/clean-html.js
+++ utils/clean-html.js
@@ -1,11 +1,19 @@
 'use strict';
-const fs = require('fs');
 
-const getData = () => {
-  let data = fs.readFileSync('./index.html', 'utf8');
-  
-  return data || [];
-}
-  
-const data = getData();
-console.log(data)
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+//joining path of directory 
+const directoryPath = path.join(__dirname, './');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach((file) => {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
