a1f5c7b4b45bcab213596dc66b502ce83d22d02b
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-08-20 09:14:10 +0300

    Update files

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -3,24 +3,25 @@
 set -e
 # echo $FIRST_NAME
 # REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
-echo "### Login into git..."
-git config --global user.email "mezgoodle@gmail.com"
-git config --global user.name "mezgoodle"
-echo "## Initializing git repo..."
-git clone https://github.com/mezgoodle/SQL.git
-cd SQL
-echo "### Adding git remote..."
-git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/SQL.git
-echo "### Install clean-html"
-npm i -g clean-html
-echo "### Execute clean-html"
-clean-html *.html --in-place
-echo "### git add ..."
-git add .
-echo "### git commit ..."
-git commit -m "test autopep8" || true
-echo "### git status"
-git status
-echo "### Push commit"
-git push format master 
-echo "Work is done"
+node utils/clean-html.js
+# echo "### Login into git..."
+# git config --global user.email "mezgoodle@gmail.com"
+# git config --global user.name "mezgoodle"
+# echo "## Initializing git repo..."
+# git clone https://github.com/mezgoodle/SQL.git
+# cd SQL
+# echo "### Adding git remote..."
+# git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/SQL.git
+# echo "### Install clean-html"
+# npm i -g clean-html
+# echo "### Execute clean-html"
+# clean-html *.html --in-place
+# echo "### git add ..."
+# git add .
+# echo "### git commit ..."
+# git commit -m "test autopep8" || true
+# echo "### git status"
+# git status
+# echo "### Push commit"
+# git push format master 
+# echo "Work is done"
--- utils/clean-html.js
+++ utils/clean-html.js
@@ -6,14 +6,14 @@ const fs = require('fs');
 //joining path of directory 
 const directoryPath = path.join(__dirname, './');
 //passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach((file) => {
-        // Do whatever you want to do with the file
-        console.log(file); 
-    });
+fs.readdir(directoryPath, (err, files) => {
+  //handling error
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  } 
+  //listing all files using forEach
+  files.forEach((file) => {
+    // Do whatever you want to do with the file
+    console.log(file); 
+  });
 });
