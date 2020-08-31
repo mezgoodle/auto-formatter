fac00ed781663a592402ce6d04315cb1a55fa941
Nyukers <26322892+nyukers@users.noreply.github.com>
2020-08-31 12:46:57 +0300

    Update python.sh

--- action/python.sh
+++ action/python.sh
@@ -8,19 +8,19 @@ echo "Start python.sh"
 echo "----"
 echo "First Tree execution"
 echo "----"
-tree -L 1
+#tree -L 1
 echo "Install packages"
 echo "----"
 pip install sync-folders flask
 pip freeze > requirements.txt
 echo "Second Tree execution"
 echo "----"
-tree -L 1
+#tree -L 1
 echo "Output to requirements.txt"
 echo "----"
 cat requirements.txt
-echo "Output 123.py"
+#echo "Output 123.py"
 echo "----"
-cat 123.py
+#cat 123.py
 echo "End of python.sh"
 #echo "Hello Max"
