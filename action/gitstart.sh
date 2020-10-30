e48f293ed0e6569dfc5a3c9bd925011939b6a46a
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-10-30 14:57:21 +0200

    Update gitstart.sh

--- action/gitstart.sh
+++ action/gitstart.sh
@@ -3,17 +3,18 @@
 set -e
 # echo $FIRST_NAME
 # REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+REPO_FULLNAME=SQL
 
 echo "### Login into git..."
 git config --global user.email "mezgoodle@gmail.com"
 git config --global user.name "mezgoodle"
 echo "## Initializing git repo..."
-git clone https://github.com/mezgoodle/SQL.git
+git clone https://github.com/mezgoodle/$REPO_FULLNAME.git
 # echo "Nothing here, only testing"
 # ls
-cd SQL
+cd $REPO_FULLNAME
 echo "### Adding git remote..."
-git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/SQL.git
+git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/$REPO_FULLNAME.git
 echo "### Install clean-html"
 npm i -g clean-html
 echo "### Install sync-folders"
@@ -22,7 +23,7 @@ echo "### Execute clean-html"
 # ls
 cd ../
 python3 utils/clean-html.py
-cd SQL
+cd $REPO_FULLNAME
 echo "### git add ..."
 git add .
 echo "### git commit ..."
