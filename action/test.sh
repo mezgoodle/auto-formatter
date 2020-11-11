70641791f2dac4c3bad054f21f92f2c5c4190a25
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-11-11 09:52:41 +0200

    #19 Create test.sh

---
+++ action/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+REPO_FULLNAME=SQL
+
+echo "### Login into git..."
+git config --global user.email "mezgoodle@gmail.com"
+git config --global user.name "mezgoodle"
+echo "## Initializing git repo..."
+git clone https://github.com/mezgoodle/$REPO_FULLNAME.git
+cd $REPO_FULLNAME
+echo "### Adding git remote..."
+git remote add format https://x-access-token:$GITHUB_TOKEN@github.com/mezgoodle/$REPO_FULLNAME.git
+echo "### Install clean-html"
+npm i -g clean-html
+echo "### Install sync-folders"
+pip install sync-folders
+echo "### git add ..."
+git add .
+echo "### git commit ..."
+git commit -m "test on Travis CI" || true
+echo "### git status"
+git status
+echo "### Push commit"
+git push format master 
+echo "Work is done"
