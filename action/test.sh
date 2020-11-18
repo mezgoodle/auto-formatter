5280f72259216db12de4ca144e89418632b53a38
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-11-18 20:57:05 +0200

    #19 #11 Prepare the final finals for testing

--- action/test.sh
+++ action/test.sh
@@ -9,18 +9,18 @@ git config --global hub.protocol https
 echo "## Initializing git repo..."
 git clone https://github.com/mezgoodle/SQL.git
 cd SQL
-echo "### Install clean-html"
+echo "### Install clean-html..."
 npm i -g clean-html
-echo "### Install sync-folders"
+echo "### Install sync-folders..."
 pip install sync-folders
-echo "### Cat index.sql"
+echo "### Cat index.sql..."
 cat index.sql
-echo "### git add ..."
+echo "### git add..."
 git add .
-echo "### git commit ..."
+echo "### git commit..."
 git commit -m "test on Travis CI" || true
-echo "### git status"
+echo "### git status..."
 git status
-echo "### Push commit"
+echo "### Push commit..."
 git push origin master || true
 echo "Work is done"
