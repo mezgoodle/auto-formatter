25e2a8f1451f21078676dedefa732c7a0e02ae5b
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-12-31 14:33:11 +0200

    Some updates

--- action/git.sh
+++ action/git.sh
@@ -75,7 +75,7 @@ then
     echo "Create config file"
     echo "{\"extends\": \"stylelint-config-standard\"}" > .stylelintrc.json
     echo "Execute stylelint"
-    stylelint "**/*.css" --fix
+    sh stylelint "**/*.css" --fix
     echo "End of css.sh"
 else
     echo "CSS is skipping"
