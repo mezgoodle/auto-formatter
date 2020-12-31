9d0050f0c279824371577b71e855c672346615ed
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-12-31 14:34:06 +0200

    Update git.sh

--- action/git.sh
+++ action/git.sh
@@ -91,7 +91,7 @@ then
     npm install markdownlint
     npm install -g markdownlint-clin
     echo "Execute markdownlinter"
-    markdownlint **/*.md --ignore node_modules --fix
+    sh markdownlint **/*.md --ignore node_modules --fix
     echo "End of md.sh"
 
 else
