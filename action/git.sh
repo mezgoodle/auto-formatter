43841f1dc2ff8461d92bfa5b071cf919c49a6767
Nyukers <nyukers@gmail.com>
2021-06-12 12:40:35 +0300

    Update git.sh

--- action/git.sh
+++ action/git.sh
@@ -10,7 +10,7 @@ read -a strarr <<< $REPO_FULLNAME # Reading str as an array as tokens separated 
 REPO_SHORTNAME=${strarr[1]}
 IFS=$"\n"
 
-REPO_FULLNAME="auto-formatter"
+REPO_FULLNAME="mezgoodle/auto-formatter"
 #################
 # Work with git #
 #################
